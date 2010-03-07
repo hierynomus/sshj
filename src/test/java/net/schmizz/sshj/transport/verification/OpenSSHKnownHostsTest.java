0df02a3ff9937e529dac981ff250487209e8e10a
Shikhar Bhushan <shikhar@schmizz.net>
2010-03-07 12:46:58 +0100

    make name of test match class name

--- src/test/java/net/schmizz/sshj/transport/verification/OpenSSHKnownHostsTest.java
+++ src/test/java/net/schmizz/sshj/transport/verification/OpenSSHKnownHostsTest.java
@@ -30,7 +30,7 @@ import java.security.PublicKey;
 
 import static org.junit.Assert.assertTrue;
 
-public class KnownHostsTest {
+public class OpenSSHKnownHostsTest {
 
     // static {
     // BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%d [%-15.15t] %-5p %-30.30c{1} - %m%n")));
@@ -39,17 +39,20 @@ public class KnownHostsTest {
     private OpenSSHKnownHosts kh;
 
     @Before
-    public void setUp() throws IOException, GeneralSecurityException {
+    public void setUp()
+            throws IOException, GeneralSecurityException {
         kh = new OpenSSHKnownHosts(new File("src/test/resources/known_hosts"));
     }
 
     @Test
-    public void testLocalhostEntry() throws UnknownHostException, GeneralSecurityException {
+    public void testLocalhostEntry()
+            throws UnknownHostException, GeneralSecurityException {
 
     }
 
     @Test
-    public void testSchmizzEntry() throws UnknownHostException, GeneralSecurityException {
+    public void testSchmizzEntry()
+            throws UnknownHostException, GeneralSecurityException {
         final PublicKey key = KeyUtil
                 .newRSAPublicKey(
                         "e8ff4797075a861db9d2319960a836b2746ada3da514955d2921f2c6a6c9895cbd557f604e43772b6303e3cab2ad82d83b21acdef4edb72524f9c2bef893335115acacfe2989bcbb2e978e4fedc8abc090363e205d975c1fdc35e55ba4daa4b5d5ab7a22c40f547a4a0fd1c683dfff10551c708ff8c34ea4e175cb9bf2313865308fa23601e5a610e2f76838be7ded3b4d3a2c49d2d40fa20db51d1cc8ab20d330bb0dadb88b1a12853f0ecb7c7632947b098dcf435a54566bcf92befd55e03ee2a57d17524cd3d59d6e800c66059067e5eb6edb81946b3286950748240ec9afa4389f9b62bc92f94ec0fba9e64d6dc2f455f816016a4c5f3d507382ed5d3365",
