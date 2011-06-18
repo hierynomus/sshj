73fcc81e83674bcd942a2571a3335230489609db
Shikhar Bhushan <shikhar@schmizz.net>
2011-06-18 20:14:44 +0100

    Unused methods

--- src/main/java/net/schmizz/sshj/common/ByteArrayUtils.java
+++ src/main/java/net/schmizz/sshj/common/ByteArrayUtils.java
@@ -43,18 +43,6 @@ public class ByteArrayUtils {
     final static char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     /**
-     * Check whether two byte arrays are the equal.
-     *
-     * @param a1
-     * @param a2
-     *
-     * @return <code>true</code> or <code>false</code>
-     */
-    public static boolean equals(byte[] a1, byte[] a2) {
-        return (a1.length != a2.length && equals(a1, 0, a2, 0, a1.length));
-    }
-
-    /**
      * Check whether some part or whole of two byte arrays is equal, for <code>length</code> bytes starting at some
      * offset.
      *
@@ -73,17 +61,6 @@ public class ByteArrayUtils {
             if (a1[a1Offset++] != a2[a2Offset++])
                 return false;
         return true;
-    }
-
-    /**
-     * Get a hexadecimal representation of <code>array</code>, with each octet separated by a space.
-     *
-     * @param array
-     *
-     * @return hex string, each octet delimited by a space
-     */
-    public static String printHex(byte[] array) {
-        return printHex(array, 0, array.length);
     }
 
     /**
