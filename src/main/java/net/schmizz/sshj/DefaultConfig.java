1ff4772f3f8e45c83b2ce615442a25aecc487a9d
Shikhar Bhushan <shikhar@schmizz.net>
2011-12-20 11:12:35 +0000

    update id string

--- src/main/java/net/schmizz/sshj/DefaultConfig.java
+++ src/main/java/net/schmizz/sshj/DefaultConfig.java
@@ -91,7 +91,7 @@ public class DefaultConfig
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private static final String VERSION = "SSHJ_0_6_0";
+    private static final String VERSION = "SSHJ_0_7";
 
     public DefaultConfig() {
         setVersion(VERSION);
