504637099db302a32dd0bc05a3814c1bde553b37
Shikhar Bhushan <shikhar@schmizz.net>
2012-04-05 12:07:34 -0400

    copyright

--- src/main/java/net/schmizz/sshj/xfer/scp/SCPDownloadClient.java
+++ src/main/java/net/schmizz/sshj/xfer/scp/SCPDownloadClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010, 2011 sshj contributors, Cyril Ledru
+ * Copyright 2010-2012 sshj contributors, Cyril Ledru
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
