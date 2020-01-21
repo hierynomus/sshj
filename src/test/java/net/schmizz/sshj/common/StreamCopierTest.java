338c3e39cbeed05179be41a92d0750411da0664c
Michiel ten Hagen <michiel.hagen@ihsmarkit.com>
2020-01-21 16:38:30 +0100

    Add license header

--- src/test/java/net/schmizz/sshj/common/StreamCopierTest.java
+++ src/test/java/net/schmizz/sshj/common/StreamCopierTest.java
@@ -1,5 +1,19 @@
+/*
+ * Copyright (C)2009 - SSHJ Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.schmizz.sshj.common;
-
 
 import org.junit.Test;
 
@@ -23,12 +37,12 @@ public class StreamCopierTest {
         OutputStream outputStream = new ByteArrayOutputStream();
         LoggerFactory loggerFactory = mock(LoggerFactory.class);
 
-        org.slf4j.Logger logger= mock(org.slf4j.Logger.class);
+        org.slf4j.Logger logger = mock(org.slf4j.Logger.class);
         when(loggerFactory.getLogger(StreamCopier.class)).thenReturn(logger);
-        StreamCopier streamCopier = new StreamCopier(inputStream,outputStream,loggerFactory);
+        StreamCopier streamCopier = new StreamCopier(inputStream, outputStream, loggerFactory);
 
-        long copied =streamCopier.copy();
-        assertThat(copied,is(1024l));
+        long copied = streamCopier.copy();
+        assertThat(copied, is(1024L));
 
         verify(logger).debug(contains("1.0 KiB transferred"));
     }
