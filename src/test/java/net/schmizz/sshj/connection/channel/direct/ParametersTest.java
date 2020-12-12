ee68e0a8e655a5cd6d2881b254e1ca6ce0ae4310
stefan <stefan@greyhair.lan>
2020-12-12 20:03:12 +0100

    Adopt project's import order (#653)
    - copied by import order of 'SocketStreamCopyMonitorTest'

--- src/test/java/net/schmizz/sshj/connection/channel/direct/ParametersTest.java
+++ src/test/java/net/schmizz/sshj/connection/channel/direct/ParametersTest.java
@@ -1,14 +1,11 @@
 package net.schmizz.sshj.connection.channel.direct;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class ParametersTest {
 
