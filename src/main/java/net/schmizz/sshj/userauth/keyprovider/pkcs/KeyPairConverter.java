beb0a4e31e615736477f459b528b40ec6e01ca45
exceptionfactory <exceptionfactory@gmail.com>
2021-08-17 07:53:43 -0500

    Corrected copyright year to match existing license headers

--- src/main/java/net/schmizz/sshj/userauth/keyprovider/pkcs/KeyPairConverter.java
+++ src/main/java/net/schmizz/sshj/userauth/keyprovider/pkcs/KeyPairConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2021 - SSHJ Contributors
+ * Copyright (C)2009 - SSHJ Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
