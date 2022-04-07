f47aa9e21cdd2ef283cd89a5eeea2c236cf5d7a1
Brent Tyler <brent.tyler@gmx.com>
2022-04-07 11:14:20 -0500

    Licensing

--- src/test/java/net/schmizz/sshj/sftp/SFTPFileTransferTest.java
+++ src/test/java/net/schmizz/sshj/sftp/SFTPFileTransferTest.java
@@ -1,6 +1,17 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C)2009 - SSHJ Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package net.schmizz.sshj.sftp;
 
@@ -10,7 +21,6 @@ import java.io.File;
 import java.io.IOException;
 import net.schmizz.sshj.SSHClient;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
